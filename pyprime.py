bead01e5f9856cdfd7f08e377a24e27370b9533f
Marinkovich77 <G00387874@gmit.it>
2020-02-15 22:13:16 +0000

    First algoritm

---
+++ pyprime.py
@@ -0,0 +1,26 @@
+# Mario Stolba
+# Computing the primes
+
+# My list of primes
+P = []
+
+# Loop through all of the numbers 
+# we`re checking for primality
+for i in range(2,100):
+    #Assume that i is a prime
+    isprime = True
+    # Look through all values from 2 up to
+    # but not including i
+    for j in range(2, i):
+        # See if j divides i
+        if i % j == 0:
+            # If it does, i isn`t prime, so exit
+            # the loop and indicate it`s not prime
+            isprime = False
+            break
+    # If i is prime, then append to P
+    if isprime:
+        P.append(i)
+
+# Print out our list
+print(P)
