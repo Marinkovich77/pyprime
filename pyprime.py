f8e23c8c41b8635bae5037904bcdc22723675cc5
Marinkovich77 <G00387874@gmit.it>
2020-02-16 08:07:26 +0000

    Make prime generator more efficient

--- pyprime.py
+++ pyprime.py
@@ -6,12 +6,12 @@ P = []
 
 # Loop through all of the numbers 
 # we`re checking for primality
-for i in range(2,100):
+for i in range(2,100000):
     #Assume that i is a prime
     isprime = True
     # Look through all values from 2 up to
     # but not including i
-    for j in range(2, i):
+    for j in P:
         # See if j divides i
         if i % j == 0:
             # If it does, i isn`t prime, so exit
